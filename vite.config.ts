d6a06ac3cfce20d6ece156412a92310acafc5647
Mahmoud Fayez <mahmoudfayez4100@gmail.com>
2026-09-24 15:56:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-data/',
   plugins: [react()],
   resolve: {
     alias: {
